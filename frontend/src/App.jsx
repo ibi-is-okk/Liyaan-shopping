47a32b311bf2d76fb7190687324df6b612baf2d7
Ibrahim <ibi8f2088@gmail.com>
2026-03-31 23:39:01 +0500

    Admin api logic added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuth } from "./context/AuthContext";
 
 import HomePage          from "./pages/HomePage";
 import LoginPage         from "./pages/LoginPage";
@@ -15,6 +16,7 @@ import WishlistPage      from "./pages/WishlistPage";
 import CartPage          from "./pages/CartPage";
 import CheckoutPage      from "./pages/CheckoutPage";
 import OrderSuccessPage  from "./pages/OrderSuccessPage";
+//import AdminDashboard    from "./pages/AdminDashboard";
 
 import "./styles/variables.css";
 import "./styles/global.css";
@@ -28,6 +30,13 @@ function Layout({ children }) {
     </>
   );
 }
+const AdminRoute = ({ children }) => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" replace />;
+  if (!user.isAdmin) return <Navigate to="/" replace />;
+  return children;
+};
+
 
 export default function App() {
   return (
@@ -42,7 +51,8 @@ export default function App() {
             <Route path="/catalogue"  element={<Layout><CataloguePage /></Layout>} />
             <Route path="/trending"   element={<Layout><TrendingPage /></Layout>} />
             <Route path="/product/:id" element={<Layout><ProductDetailPage /></Layout>} />
-
+            {/* <Route path="/admin"       element={<AdminRoute><AdminDashboard /></AdminRoute>} /> */}
+            
             {/* Protected */}
             <Route path="/wishlist" element={<Layout><ProtectedRoute><WishlistPage /></ProtectedRoute></Layout>} />
             <Route path="/cart"     element={<Layout><ProtectedRoute><CartPage /></ProtectedRoute></Layout>} />
