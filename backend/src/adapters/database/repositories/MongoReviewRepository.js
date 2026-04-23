83f4ef9812e97f4370a4c6250940bc2a0d3b9842
Ibrahim <ibi8f2088@gmail.com>
2026-04-23 19:49:03 +0500

    added repositories logic

---
+++ backend/src/adapters/database/repositories/MongoReviewRepository.js
@@ -0,0 +1,27 @@
+const IReviewRepository = require("../../../ports/IReviewRepository");
+const ReviewModel = require("../models/ReviewModel");
+const ProductModel = require("../models/ProductModel");
+
+class MongoReviewRepository extends IReviewRepository {
+  async findByProduct(productId) {
+    return ReviewModel.find({ product: productId }).sort({ createdAt: -1 });
+  }
+
+  async create(data) {
+    const review = await ReviewModel.create(data);
+    // Update product rating
+    const reviews = await ReviewModel.find({ product: data.product });
+    const avg = reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length;
+    await ProductModel.findByIdAndUpdate(data.product, {
+      rating: Math.round(avg * 10) / 10,
+      reviewCount: reviews.length,
+    });
+    return review;
+  }
+
+  async findByUserAndProduct(userId, productId) {
+    return ReviewModel.findOne({ user: userId, product: productId });
+  }
+}
+
+module.exports = MongoReviewRepository;
